c489617aeb879a3c02b4b5f61142eb54c840d2b3
Shikhar Gupta <127095494+shikhartheone@users.noreply.github.com>
2025-04-19 06:23:45 +0530

    Update App 2.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function App() {
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <h1 className="text-4xl font-bold">Hello Shikhar</h1
+      <h1 className="text-4xl font-bold">Hello Shikhar</h1>
     </div>
   );
 }
